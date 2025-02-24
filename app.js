9dcb2f808a0c6c48c7723ca489758249b135c75e
mengji-dhanush <mengji.dhanush16@gmail.com>
2025-02-24 10:03:16 +0530

    mongo atlas change

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 const express = require("express");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const path = require("path");
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+
+const uri = process.env.ATLASDB_URL;
+async function connectDB() {
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB Atlas connected successfully!");
+  } catch (err) {
+    console.error("MongoDB Atlas connection error:", err);
+  }
+}
+
+connectDB();
 
 app.set("view engine", "ejs");
 app.set(path.join(__dirname, "views"));
 
+const feedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  feedback: {
+    type: String,
+    required: true,
+  },
+});
+
+const Feedback = new mongoose.model("Feedback", feedbackSchema);
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+const store = MongoStore.create({
+  mongoUrl: uri,
+  crypto: {
+    secret: "mySecretCode",
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("some error occured in mongo session store");
+});
+
 app.use(
   session({
+    store: store,
     secret: "mySecretCode",
     resave: false,
     saveUninitialized: true,
@@ -34,9 +74,20 @@ app.get("/feedback", (req, res) => {
   res.render("feedback.ejs");
 });
 
-app.post("/feedback", (req, res) => {
-  req.flash("success", "your feedback has been recorded");
-  res.redirect("/");
+app.post("/feedback", async (req, res) => {
+  try {
+    let { name, feedback } = req.body;
+    let feedback1 = new Feedback({
+      name: name,
+      feedback: feedback,
+    });
+    await feedback1.save();
+    req.flash("success", "your feedback has been recorded");
+    res.redirect("/");
+  } catch (err) {
+    req.flash("error", "some error occured");
+    res.redirect("/");
+  }
 });
 
 app.listen(8080, () => {
